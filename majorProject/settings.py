fcee9cb56c5bb28955f2021da82612b3c4081cc1
AchyutRpi <achyutkneupane@gmail.com>
2019-10-22 22:13:40 +0545

    Database added POSTGRES

--- majorProject/settings.py
+++ majorProject/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'majorProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'achyut',
+        'PASSWORD': 'neupane1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
